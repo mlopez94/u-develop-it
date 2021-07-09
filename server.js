9a54e711935eed0966045ad5f94c378dfcad846e
mlopez <lopezmatthew87@gmail.com>
2021-07-09 13:17:48 -0400

    adding queries

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'password',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
 app.get('/', (req, res) => {
     res.json({
       message: 'Hello World'
     });
+  });
+
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
   });
 
   // Default response for any other request (Not Found)
